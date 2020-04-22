c3cd69071f6e86194661f68724b646bedc06c179
Jonathan Kang <jon.m.kang@gmail.com>
2020-04-22 14:18:27 -0400

    constructed a github repo search using API

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// const searchURL = 'https://api.github.com/users/jonmkang/repos'
+
+function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      const searchUser = $('#js-search-user').val();
+      getRepo(searchUser);
+    });
+}
+
+function getRepo(query){
+    const url = `https://api.github.com/users/${query}/repos`
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        // .catch(err => console.log('Something went wrong'))
+}
+
+function displayResults(responseJson){
+    console.log(responseJson);
+    let i = 0;
+    $('#results-list').empty();
+    for(i = 0; i < responseJson.length; i++){
+        $('#results-list').append(
+            `<li><a href="${responseJson[i].html_url}">${responseJson[i].name}</a>`
+        )
+    }
+    $('#results').removeClass('hidden');
+};
+  
+$(watchForm);
